1ac7cce702e7d09f179a44310579da77579c9ef3
Rick van de Loo <rickvandeloo@gmail.com>
2014-01-26 15:52:59 +0100

    revert back to original size windows on stop_float()

--- static/js/float.js
+++ static/js/float.js
@@ -3,6 +3,24 @@ var dc = 0;
 var zc = { n : 1000, d : 2000 };
 var store_pos = [];
 var prop;
+
+function get_ec(el) {
+	var flid = el.attr('flid');
+	var cur_ec = false;
+	var el_index;
+	for (el_index=1; el_index<store_pos.length; ++el_index) {
+		if (!cur_ec) {
+			cur_ec = store_pos[el_index]['identifier'] == flid ? flid : false;
+		}
+	}
+	cur_ec = cur_ec ? cur_ec : ++dc;
+	if (el.attr('flid') === 'undefined') {
+		el.attr('flid', cur_ec);
+		store_pos[cur_ec]['identifier'] = cur_ec;
+	}	
+	return cur_ec;
+}
+
 function start_float() {
 	$('body').css('overflow', 'hidden');
 
@@ -15,7 +33,7 @@ function start_float() {
 			x : el.offset().left,
 			y : el.offset().top,
 			w : el.width(),
-			h : el.height()
+			h : el.height(),
 		}
 
 		// store original position of windows for animation
@@ -23,12 +41,11 @@ function start_float() {
 		store_pos[cur_ec] = store_pos[cur_ec] === undefined ? new Array() : store_pos[cur_ec];
 		store_pos[cur_ec]['identifier'] = cur_ec;
 		store_pos[cur_ec]['static'] = store_pos[cur_ec]['static'] ? store_pos[cur_ec]['static'] : el.clone();
-		store_pos[cur_ec]['left'] = store_pos[cur_ec]['left'] ? store_pos[cur_ec]['left'] : prop.x;
-		store_pos[cur_ec]['top'] = store_pos[cur_ec]['top'] ? store_pos[cur_ec]['top'] : prop.y;
+		store_pos[cur_ec]['l'] = store_pos[cur_ec]['l'] ? store_pos[cur_ec]['l'] : prop.x;
+		store_pos[cur_ec]['t'] = store_pos[cur_ec]['t'] ? store_pos[cur_ec]['t'] : prop.y;
 
 		if (el.attr('flid') !== 'undefined') el.attr('flid', cur_ec);
 		el.css({width: prop.w, height: prop.h, left: prop.x, top: prop.y, position:'absolute'});
-		el.resizable();
 		console.log(el.find('h2'));
 	});
 	$('h2').css('cursor', 'move');
@@ -49,19 +66,7 @@ function start_float() {
 		zc[t]++;
 		el.css({'z-index': zc[t]});
 
-		var flid = el.attr('flid');
-		var cur_ec = false;
-		var el_index;
-		for (el_index=1; el_index<store_pos.length; ++el_index) {
-			if (!cur_ec) {
-				cur_ec = store_pos[el_index]['identifier'] == flid ? flid : false;
-			}
-		}
-		cur_ec = cur_ec ? cur_ec : ++dc;
-		if (el.attr('flid') === 'undefined') {
-			el.attr('flid', cur_ec);
-			store_pos[cur_ec]['identifier'] = cur_ec;
-		}	
+		var cur_ec = get_ec(el);
 
 		prop = {
 			x : el.offset().left,
@@ -98,13 +103,26 @@ function start_float() {
 			$(document).unbind('mousemove');
 		});
 	});
+	make_resizable();
+}
 
+function make_resizable(){
 	$('ul.modules>li, .dialog').hover(function(){
 		$(this).find('h2').css('cursor', 'move');
+		var res = $(this).children('div');
+		if (!res.hasClass('ui-resizable')){
+			fdiv = $('>div:not(.placeholder)', $(this));
+			prop.dw = fdiv.width(),
+			prop.dh = fdiv.height()
+			var cur_ec = get_ec($(this));
+			store_pos[cur_ec]['w'] = store_pos[cur_ec]['w'] ? store_pos[cur_ec]['w'] : prop.dw;
+			store_pos[cur_ec]['h'] = store_pos[cur_ec]['h'] ? store_pos[cur_ec]['h'] : prop.dh;
+			res.resizable();
+		}
 	});
 }
+
 function stop_float() {
-	$('body').css('overflow', 'visible');
 	
 	// put the windows back in docked position
 	
@@ -117,7 +135,20 @@ function stop_float() {
 				el.replaceWith(store_pos[cbi]['static']);
 			}
 		}(el, el_index);
-		el.animate({left:store_pos[el_index]['left'], top: store_pos[el_index]['top']}, 'slow', restore_relative);
+		$('>div:not(.placeholder)', el).animate({
+			width: store_pos[el_index]['w'],
+			height: store_pos[el_index]['h']
+		},{
+			duration : 500,
+			queue : false
+		});
+		el.animate({
+			left:store_pos[el_index]['l'], 
+			top: store_pos[el_index]['t'], 
+		}, 
+			'slow', 
+			restore_relative
+		);
 	}
 	$('ul.modules>li, .dialog').unbind('mousedown').unbind('mouseenter').unbind('mouseleave');
 }
@@ -132,7 +163,9 @@ $('#settings_icon').on('click', function(){
 		}, 500);
 	}
 	else {
-		start_float();
+		setTimeout(function() { 
+			start_float();
+		}, 500);
 	}
 });
-start_float();
+//start_float();
